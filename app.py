e5e18eefcddcb13a34b6a04d0a5148915f48f610
William Jensen <williambjensen01@gmail.com>
2021-07-27 11:09:02 -0600

    changed hero database url to work with sqlalchemy

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ app = Flask(__name__)
 CORS(app)
 
 app.debug = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+uri = os.getenv('DATABASE_URL')
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 
 db = SQLAlchemy(app)
